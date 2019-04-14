364298b279512a361b46876365c2f05eb3ed8f9b
NewsDLee <li_420211707@163.com>
2019-04-14 16:13:18 +0800

    add prehandle method

--- src/main/java/com/volunteer/intecepter/LoginIntecepter.java
+++ src/main/java/com/volunteer/intecepter/LoginIntecepter.java
@@ -1,5 +1,8 @@
 package com.volunteer.intecepter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
@@ -9,6 +12,13 @@ import org.springframework.web.servlet.HandlerInterceptor;
  * @desc 登录拦截
  *
  */
-public class LoginIntecepter implements HandlerInterceptor{
+public class LoginIntecepter implements HandlerInterceptor {
 
+	// 进入 Handler方法之前执行
+	// 用于身份认证、身份授权
+	// 比如身份认证，如果认证通过表示当前用户没有登陆，需要此方法拦截不再向下执行
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return false;
+	}
 }
