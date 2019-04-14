a80b4e5f9b35f872ce9555455d41fb4a3587d561
NewsDLee <li_420211707@163.com>
2019-04-14 16:11:43 +0800

    add login intecepter

---
+++ src/main/java/com/volunteer/intecepter/LoginIntecepter.java
@@ -0,0 +1,14 @@
+package com.volunteer.intecepter;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * 
+ * @author NewsDLee
+ * @date 2019年4月14日16:10:45
+ * @desc 登录拦截
+ *
+ */
+public class LoginIntecepter implements HandlerInterceptor{
+
+}
