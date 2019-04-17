1940debbcaf02a85967dc0924663e223c48c2895
yuan.shi <yuan.shi1@baozun.com>
2019-04-17 17:36:31 +0800

    获取微信用户信息

---
+++ src/main/java/com/volunteer/constant/WxLoginConstant.java
@@ -0,0 +1,22 @@
+package com.volunteer.constant;
+
+/**
+ * 
+ * @author yuan
+ * @date 2019年4月14日16:23:34
+ * @desc 登录常亮
+ */
+public class WxLoginConstant {
+    /**
+     * 存放在session中的用户信息
+     */
+    public static final String WECHAT_USERINFO_SESSION = "WechatUserInfoSession";
+    /**
+     * 存放在redis中的用户信息
+     */
+    public static final String WECHAT_USERINFO = "WechatUserInfo";
+    /**
+     * 微信接口成功code
+     */
+    public static final Integer WECHAT_ACCESS_SUCCESS = 27000;
+}
--- src/main/java/com/volunteer/web/controller/login/UserLoginController.java
+++ src/main/java/com/volunteer/web/controller/login/UserLoginController.java
@@ -6,6 +6,7 @@ import com.feilong.core.Validator;
 import com.feilong.servlet.http.RequestUtil;
 import com.volunteer.cache.manager.CacheManager;
 import com.volunteer.common.WechatMessage;
+import com.volunteer.constant.WxLoginConstant;
 import com.volunteer.utils.HttpsUtils;
 import com.volunteer.utils.PropBean;
 import net.sf.json.JSONObject;
@@ -41,35 +42,36 @@ public class UserLoginController {
      * @return
      */
     @RequestMapping(value = "/weChatLogin")
-    public String mobileWechatLogin(HttpServletRequest request, HttpServletResponse response, @RequestParam(value = "code",required = true) String code){
+    public String mobileWechatLogin(HttpServletRequest request, HttpServletResponse response, @RequestParam(value = "code", required = true) String code) {
         Map<String, String> parameterMap = RequestUtil.getParameterSingleValueMap(request);
         //请求链接
         String returnUrl = parameterMap.get("returnUrl");
-        try{
-            if (Validator.isNullOrEmpty(returnUrl)){
+        try {
+            if (Validator.isNullOrEmpty(returnUrl)) {
                 returnUrl = "";
             }
             //通过code换取用户信息--先从缓存中获取，没有就从第三方获取
-            String httpResponse = cacheManager.getValue("WECHAT_MESSAGE" + code);
-            if (httpResponse == null){
+            String httpResponse = cacheManager.getValue(WxLoginConstant.WECHAT_USERINFO + code);
+            if (httpResponse == null) {
                 httpResponse = getWechatMemberInfo(code);
             }
-            if (Validator.isNullOrEmpty(httpResponse)){
+            if (Validator.isNullOrEmpty(httpResponse)) {
                 return "redirect:" + returnUrl;
             }
             WechatMessage wechatMessage = JSON.parseObject(httpResponse, WechatMessage.class);
             //查询信息失败
-            if (wechatMessage.getErrcode() != 0){
+            if (wechatMessage.getErrcode() != 0) {
                 return "redirect:" + returnUrl;
             }
             //查询成功
             //如果是微信会员 ---将信息缓存起来
-            if (cacheManager.getValue("WECHAT_MESSAGE" + code) == null){
-                cacheManager.setValue("WECHAT_USERINFO" + code, httpResponse, TimeInterval.SECONDS_PER_HOUR * 2);
+            if (cacheManager.getValue(WxLoginConstant.WECHAT_USERINFO + code) == null) {
+                cacheManager.setValue(WxLoginConstant.WECHAT_USERINFO + code, httpResponse, TimeInterval.SECONDS_PER_HOUR * 2);
             }
-            request.getSession().setAttribute("WECHAT_USERINFO", httpResponse);
-//            return wechatOAuthSuccess(wechatMessage, request, response, returnUrl);
-        }catch (Exception e){
+            request.getSession().setAttribute(WxLoginConstant.WECHAT_USERINFO_SESSION, httpResponse);
+            //登录用户的处理方法
+            //return wechatOAuthSuccess(wechatMessage, request, response, returnUrl);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -77,24 +79,30 @@ public class UserLoginController {
 
     }
 
-    private String getWechatMemberInfo(String code){
+    private String getWechatMemberInfo(String code) {
         String httpResponse = null;
-        try{
-        String appid = prop.getAppid();
-        String appsercet = prop.getAppid();
-        String accessUrl = prop.getAccessToken()+"appid="+appid+"&secret="+appsercet+"&code="+code+"&grant_type=authorization_code";;
-        String json = HttpsUtils.sendGet(accessUrl);
-        JSONObject jsonObject = JSONObject.fromObject(json);
-        String access_token = jsonObject.getString("access_token");
-        String openid = jsonObject.getString("openid");
-        if(Validator.isNullOrEmpty(access_token)&&Validator.isNullOrEmpty(openid)){
-            return httpResponse;
-        }
-        String get_userinfo = prop.getUserinfo()+"access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN";
-        get_userinfo = get_userinfo.replace("ACCESS_TOKEN", access_token);
-        get_userinfo = get_userinfo.replace("OPENID", openid);
-        httpResponse =  HttpsUtils.sendGet(get_userinfo);
-        }catch (Exception e){
+        try {
+            //从配置文件中拿信息
+            String appid = prop.getAppid();
+            String appsercet = prop.getAppid();
+            String accessUrl = prop.getAccessToken() + "appid=" + appid + "&secret=" + appsercet + "&code=" + code + "&grant_type=authorization_code";
+            //获取accessToken，openId
+            String json = HttpsUtils.sendGet(accessUrl);
+            JSONObject jsonObject = JSONObject.fromObject(json);
+            if (WxLoginConstant.WECHAT_ACCESS_SUCCESS.equals(jsonObject.get("errcode"))) {
+                return httpResponse;
+            }
+            String access_token = jsonObject.getString("access_token");
+            String openid = jsonObject.getString("openid");
+            if (Validator.isNullOrEmpty(access_token) && Validator.isNullOrEmpty(openid)) {
+                return httpResponse;
+            }
+            //获取用户信息
+            String get_userinfo = prop.getUserinfo() + "access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN";
+            get_userinfo = get_userinfo.replace("ACCESS_TOKEN", access_token);
+            get_userinfo = get_userinfo.replace("OPENID", openid);
+            httpResponse = HttpsUtils.sendGet(get_userinfo);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return httpResponse;
