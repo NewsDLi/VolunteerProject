dc905ff83617e0ae95400752807c58c31c88081e
NewsDLee <li_420211707@163.com>
2019-04-21 01:47:29 +0800

    添加微信登录页面

--- src/main/java/com/volunteer/web/controller/ForwardController.java
+++ src/main/java/com/volunteer/web/controller/ForwardController.java
@@ -3,11 +3,31 @@ package com.volunteer.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * @author NewsDLee
+ * @date 2019年4月21日01:43:19
+ * @desc 用来做页面跳转的controller
+ *
+ */
 @Controller
 public class ForwardController {
 	
+	/**
+	 * 跳转至首页
+	 * @return
+	 */
 	@RequestMapping("/index")
 	public String toIndex(){
 		return "index";
+	}
+	
+	/**
+	 * 跳转至微信登录页面
+	 * @return
+	 */
+	@RequestMapping("/wechartLogin")
+	public String toWechartLogin(){
+		return "wechartLogin";
 	}
 }
