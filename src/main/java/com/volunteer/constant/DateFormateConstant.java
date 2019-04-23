4e8f9ad1e66c3e2035f545d36655e4e38a1b2fd7
NewsDLee <li_420211707@163.com>
2019-04-23 19:55:06 +0800

    添加线程安全日期转换工具

---
+++ src/main/java/com/volunteer/constant/DateFormateConstant.java
@@ -0,0 +1,14 @@
+package com.volunteer.constant;
+
+public class DateFormateConstant {
+
+	public static final String YEAR_MONTH_DAY_SECOND = "yyyy-MM-dd HH:mm:ss";
+	
+	public static final String YEAR_MONTH_DAY_SECOND2 = "yyyy/MM/dd HH:mm:ss";
+	
+	public static final String YEAR_MONTH_DAY_SECOND3 = "yyyy年MM月dd日 HH时mm分ss秒";
+	
+	public static final String YEAR_MONTH_DAY = "yyyy-MM-dd";
+	
+	public static final String YEAR_MONTH_DAY2 = "yyyy年MM月dd日";
+}
