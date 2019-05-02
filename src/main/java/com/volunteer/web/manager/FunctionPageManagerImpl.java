1f752c0f2c28122a833f9722352067e92a91f54b
shiyuan <447233580@qq.com>
2019-05-03 00:27:55 +0800

    shangchaun

--- src/main/java/com/volunteer/web/manager/FunctionPageManagerImpl.java
+++ src/main/java/com/volunteer/web/manager/FunctionPageManagerImpl.java
@@ -1,5 +1,6 @@
 package com.volunteer.web.manager;
 
+import com.feilong.core.Validator;
 import com.feilong.core.bean.PropertyUtil;
 import com.feilong.core.util.CollectionsUtil;
 import com.volunteer.model.BindRolePower;
@@ -29,11 +30,16 @@ public class FunctionPageManagerImpl implements FunctionPageManager{
 		BindRolePowerExample bindRolePowerExample = new BindRolePowerExample();
 		bindRolePowerExample.createCriteria().andRoleIdEqualTo(roleId);
 		List<BindRolePower> bindRolePowers = bindRolePowerMapper.selectByExample(bindRolePowerExample);
-//		CollectionsUtil.find(bindRolePowers,"powerId",)
-//		bindRolePowers
+		List<Long> powerId = CollectionsUtil.getPropertyValueList(bindRolePowers, "powerId");
+		if(Validator.isNullOrEmpty(powerId)){
+			return null;
+		}
 		UserPowerExample userPowerExample = new UserPowerExample();
-//		userPowerExample.createCriteria().andIdIn()
-//		List<UserPower> userPowers = userPowerMapper.selectByExample();
-		return null;
+		userPowerExample.createCriteria().andIdIn(powerId);
+		List<UserPower> userPowers = userPowerMapper.selectByExample(userPowerExample);
+		if(Validator.isNullOrEmpty(userPowers)){
+			return null;
+		}
+		return userPowers;
 	}
 }
