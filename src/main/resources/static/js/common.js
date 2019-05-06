346950c63200ad3590b03904701ef32498240017
yuan.shi <yuan.shi1@baozun.com>
2019-05-06 15:10:21 +0800

    主页

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,9 +1,12 @@
 $(function(){
 	$('.homepage').click(function(){
 		// TODO
-		location.href = "/mypage";
+		location.href = "/homepage.htm";
 	})
 	$('.mine').click(function(){
 		location.href = "/mypage";
+	})
+	$('.community').click(function(){
+		location.href = "/community.htm";
 	})
 })
--- src/main/resources/static/js/function.js
+++ src/main/resources/static/js/function.js
@@ -1,43 +1,43 @@
- $(document).ready(function(){
-        // 初始化内容
-        init();
+$(document).ready(function () {
+    // 初始化内容
+    init();
 
+});
+
+function init() {
+    $.post("/function.json",
+        function (data, status) {
+            if (isEmptyObject(data)) {
+                var a = basisUrl("url");
+                var b = $.i18n.prop("url");
+                window.location.href = b + "/index";
+            }
+            var text = "";
+            for (var i = 0; i < data.userPowers.length; i++) {
+                text += "<a href=" + data.userPowers[i].powerUrl + " class='weui-grid'><div class='weui-grid__icon'><img src=" + data.userPowers[i].powerPicUrl + " alt=''></div><p class='weui-grid__label''>" + data.userPowers[i].powerName + "</p></a>";
+            }
+            $('.weui-grids').html(text);
+        });
+}
+
+function basisUrl(name) {
+
+    jQuery.i18n.properties({
+        name: 'switch',
+        path: 'config/',
+        mode: 'map',
+        encoding: 'UTF-8',
+        callback: function () {
+            console.log($.i18n.prop(name));
+        }
     });
+}
 
-    function init() {
-            $.post("/function.json",
-            function(data,status){
-             if(isEmptyObject(data)){
-             var a = basisUrl("url");
-             var b =  $.i18n.prop("url");
-                window.location.href = b+"/index";
-             }
-             var text = "";
-             for (var i=0;i<data.userPowers.length;i++)
-             {
-             text += "<a href="+data.userPowers[i].powerUrl+" class='weui-grid'><div class='weui-grid__icon'><img src="+data.userPowers[i].powerPicUrl+" alt=''></div><p class='weui-grid__label''>"+data.userPowers[i].powerName+"</p></a>";
-             }
-             $('.weui-grids').html(text);
-            });
+function isEmptyObject(obj) {
+
+    for (var key in obj) {
+        return false
     }
-
-    function basisUrl(name){
-
-jQuery.i18n.properties({
-           name:'switch',
-           path:'config/',
-           mode:'map',
-           encoding: 'UTF-8',
-           callback: function() {
-              console.log($.i18n.prop(name));
-           }
-       });
-    }
-
-    function isEmptyObject(obj){
-
-         for(var key in obj){
-              return false
-         };
-         return true
-    };
+    ;
+    return true
+};
