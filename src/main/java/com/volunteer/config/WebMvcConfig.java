4a5cd65d69db08fff3e7d63ceb6bdb99cd550e6c
yuan.shi <shang.xiang@baozun.com>
2019-05-16 16:30:18 +0800

    拦截器开启

--- src/main/java/com/volunteer/config/WebMvcConfig.java
+++ src/main/java/com/volunteer/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.volunteer.config;
 
 import com.volunteer.intecepter.LoginIntecepter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +15,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //addPathPattern后跟拦截地址，excludePathPatterns后跟排除拦截地址
-        registry.addInterceptor(new LoginIntecepter()).addPathPatterns("/**").excludePathPatterns("/weChatLogin");
+//        registry.addInterceptor(new LoginIntecepter()).addPathPatterns("/**").excludePathPatterns("/index");
+        // 注册拦截器
+        LoginIntecepter loginInterceptor = new LoginIntecepter();
+        InterceptorRegistration loginRegistry = registry.addInterceptor(loginInterceptor);
+        // 拦截路径
+        loginRegistry.addPathPatterns("/**");
+        // 排除路径
+        loginRegistry.excludePathPatterns("/");
+        loginRegistry.excludePathPatterns("/index");
+        loginRegistry.excludePathPatterns("/weChatLogin");
+        loginRegistry.excludePathPatterns("/login.json");
+        // 排除资源请求
+        loginRegistry.excludePathPatterns("/css/login/*.css");
+        loginRegistry.excludePathPatterns("/js/login/**/*.js");
+        loginRegistry.excludePathPatterns("/image/login/*.*");
+
     }
 }
--- src/main/java/com/volunteer/intecepter/LoginIntecepter.java
+++ src/main/java/com/volunteer/intecepter/LoginIntecepter.java
@@ -1,6 +1,8 @@
 package com.volunteer.intecepter;
 
-import com.volunteer.constant.WxLoginConstant;
+import com.volunteer.constant.UserConstant;
+import com.volunteer.model.UserInfo;
+import org.apache.catalina.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,20 +18,20 @@ import javax.servlet.http.HttpSession;
  */
 public class LoginIntecepter implements HandlerInterceptor {
 
-	// 暂时注释 by NewsDLee
-	// 进入 Handler方法之前执行
-	// 用于身份认证、身份授权
-	// 比如身份认证，如果认证通过表示当前用户没有登陆，需要此方法拦截不再向下执行
-//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//			throws Exception {
-//		HttpSession session = request.getSession();
-//		String user = (String) session.getAttribute(WxLoginConstant.WECHAT_USERINFO_SESSION); //获取登录的session信息
-//		if(user!=null){
-//			return true;
-//		}
-//		else{
-//			response.sendRedirect(request.getContextPath()+"/weChatLogin");  //未登录自动跳转界面
-//			return false;
-//		}
-//	}
+//	 暂时注释 by NewsDLee
+//	 进入 Handler方法之前执行
+//	 用于身份认证、身份授权
+//	 比如身份认证，如果认证通过表示当前用户没有登陆，需要此方法拦截不再向下执行
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		UserInfo user = (UserInfo) session.getAttribute(UserConstant.LOGIN_PHONE); //获取登录的session信息
+		if(user!=null){
+			return true;
+		}
+		else{
+			response.sendRedirect("/index");  //未登录自动跳转界面
+			return false;
+		}
+	}
 }
