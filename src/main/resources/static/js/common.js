438f862eb319a8a4388f9737b3902c0ae85a2d1a
yuan.shi <shang.xiang@baozun.com>
2019-05-23 13:48:41 +0800

    fuwenben

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,5 +1,17 @@
 $(function(){
-	$('.homepage').click(function(){
+   	if( window.location.pathname.indexOf("homepage") >= 0) {
+       $(".homepage").addClass("weui-bar__item_on")
+    }
+    var s=window.location.pathname.indexOf("mypage")
+     var ss  = window.location.pathname.indexOf("login.json")
+   	if( window.location.pathname.indexOf("mypage") >= 0 || window.location.pathname.indexOf("login.json") >= 0 	) {
+       $(".mine").addClass("weui-bar__item_on")
+    }
+
+   	if( window.location.pathname.indexOf("community") >= 0 ) {
+       $(".community").addClass("weui-bar__item_on")
+    }
+    $('.homepage').click(function(){
 		// TODO
 		location.href = "/homepage.htm";
 	})
